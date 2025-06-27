0907659633de2ed3329aee86b9d72d3e6e9794e8
Abdullah <bdallhalbnt@gmail.com>
2025-06-27 21:31:45 +0300

    Farmer Edit

--- Components/PageTransitionWrapper.tsx
+++ Components/PageTransitionWrapper.tsx
@@ -25,7 +25,7 @@ export default function PageTransitionWrapper({
           initial={{ opacity: 0, filter: "blur(8px)" }}
           animate={{ opacity: 1, filter: "blur(0px)" }}
           exit={{ opacity: 0, filter: "blur(8px)" }}
-          transition={{ duration: 0.4, ease: "easeInOut" }}
+          transition={{ duration: 0.8, ease: "easeInOut" }}
         >
           {children}
         </motion.div>
